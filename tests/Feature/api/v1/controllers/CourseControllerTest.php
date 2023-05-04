62ed6885d63e1e35256b3a69313255780631c430
Quisui <quiqueal.17@hotmail.com>
2023-05-04 07:40:27 -0600

    Update CourseControllerTest.php

--- tests/Feature/api/v1/controllers/CourseControllerTest.php
+++ tests/Feature/api/v1/controllers/CourseControllerTest.php
@@ -35,7 +35,6 @@ class CourseControllerTest extends TestCase
 
     public function testCoursesCanBeFilterInPresentOrNotStudents(): void
     {
-
         $withNoPresense = Course::query()
             ->where('instructor_id', $this->instructor->id)
             ->with(['students', 'instructor'])->withWhereHas('students', function ($query) {
@@ -47,8 +46,8 @@ class CourseControllerTest extends TestCase
         if ($totalNoAssist < 1) {
             $this->markTestSkipped('skipping this test');
         }
-        $response->assertStatus(200)
-            ->assertJsonCount($totalNoAssist, 'data.0.students');;
+        $response->assertStatus(200);
+        $this->assertCount($totalNoAssist, $response->json()['data'][0]['students']);
 
 
         $response = $this->getJson('/api/courses?present=false');
@@ -56,8 +55,8 @@ class CourseControllerTest extends TestCase
         if ($totalAssist < 1) {
             $this->markTestSkipped('skipping this test');
         }
-        $response->assertStatus(200)
-            ->assertJsonCount($totalAssist, 'data.0.students');
+        $response->assertStatus(200);
+        $this->assertCount($totalAssist, $response->json()['data'][0]['students']);
     }
 
     public function testPresentStatusCanBeUpdatedInCourse(): void
